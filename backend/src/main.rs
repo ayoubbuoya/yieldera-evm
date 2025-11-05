5feaaf11b1d0832e8e057666d0f3b11dd794e309
Ayoub Amer <ayouub.ameur@gmail.com>
2025-11-05 12:13:54 +0100

    feat: Add Actix web server with CORS and Swagger UI integration

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use actix_cors::Cors;
+use actix_web::{App, HttpServer, web};
+use utoipa_actix_web::AppExt;
+use utoipa_swagger_ui::SwaggerUi;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Start the http server
+    println!("Starting Http Server at http://127.0.0.1:8090");
+    println!("Starting SWAGGER Server at http://127.0.0.1:8090/swagger-ui/");
+
+    let app_state = web::Data::new(()); // Placeholder for shared application state
+
+    HttpServer::new(move || {
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allow_any_method()
+            .allow_any_header();
+
+        let (app, app_api) = App::new()
+            .wrap(cors)
+            .into_utoipa_app()
+            .app_data(web::Data::clone(&app_state))
+            .split_for_parts();
+
+        app.service(SwaggerUi::new("/swagger-ui/{_:.*}").url("/api-docs/openapi.json", app_api))
+    })
+    .bind(("127.0.0.1", 8090))?
+    .run()
+    .await
 }
